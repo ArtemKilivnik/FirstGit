ed0561c63d8e0e7bf5681235bc48dde1563671b1
ArtemKilivnik <93001301+ArtemKilivnik@users.noreply.github.com>
2021-10-28 21:26:42 +0300

    branch28_10

---
+++ New Project1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "math.h"
+#include <iomanip>
+using namespace std;
+int main()
+{
+    float y, x;
+    cout << "Input x: ";
+    cin >> x;
+    y = 0;
+    for (int n = 1; n < 6; n++)
+    {
+        y += cos(n);
+    }
+    y = (tan(x * x) + y) / x + 5;
+    cout << "Output y = " << setprecision(2) << y << endl;
+}
