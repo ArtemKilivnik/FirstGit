2635af114aaa648ef81fecc5057715b84a32f174
ArtemKilivnik <93001301+ArtemKilivnik@users.noreply.github.com>
2021-11-11 19:29:03 +0200

    branch2021

--- First Project.cpp
+++ First Project.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include "math.h"
 #include <iomanip>
-#include "windows.h"
+//new commentary
+
 using namespace std;
 int main()
 {
-    float y, x;
-    cout << "Input x: ";
-    cin >> x;
+    float y, c;
+    cout << "Input x: " << endl;
+    cin >> c;
     y = 0;
     for (int n = 1; n < 6; n++)
     {
         y += sin(n);
     }
-    y = (tan(x * x) + y) / x + 5;
-    cout << "Output y = " << setprecision(2) << y;
-    //just new commentary
+    y = (tan(c * c) + y) / c + 5;
+    cout << "Output Y = " << setprecision(2) << y << endl;
 }
